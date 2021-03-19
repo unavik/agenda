f169cccf960c2277c968ed73ea4017ca813a5811
Augusto Aragon <3626599-c-aragon1@users.noreply.gitlab.com>
2021-03-18 21:20:38 -0600

    Se agrega interfaz para el controlador de personas.

---
+++ src/main/java/com/unavik/agenda/controller/PersonController.java
@@ -0,0 +1,10 @@
+package com.unavik.agenda.controller;
+
+import com.unavik.agenda.dto.PersonDto;
+
+import java.util.List;
+
+public interface PersonController {
+
+    List<PersonDto> getPersons();
+}
