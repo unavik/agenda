5dceb8de337b3531a0f387396dc8c60a091caa28
Augusto Aragon <3626599-c-aragon1@users.noreply.gitlab.com>
2021-03-18 21:19:36 -0600

    Agregando datos via sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into person(first_name, last_name, birth_day) values ('Charly', 'Brown', '1985-11-28');
+insert into person(first_name, last_name, birth_day) values ('Peter', 'Parker', '2001-08-10');
+insert into person(first_name, last_name, birth_day) values ('Barry', 'Allen', '2001-10-10');
+insert into person(first_name, last_name, birth_day) values ('Arthur', 'Curry', '1990-01-10');
+
